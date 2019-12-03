20bedc10007925f090756fdea1aed55e1a65498c
ortlaz <anastasea.com@gmail.com>
2019-12-03 16:40:26 +0300

    Сделала меню

--- main.cpp
+++ main.cpp
@@ -6,30 +6,69 @@ using namespace std;
 
 int main()
 {
-    //TODO:сделать меню
-
-
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     Hash_Table<string,string> HT;
-    HT.push("МИРЭА","001");
-    HT.push("МИРЭА_2","102");
-    HT.push("МИТХТ","111");
-    HT.push("МИТХТ","210");
 
-    HT.output();
-    HT.findEl();
+     int var;
 
-    cout<<"\nСписок индексов\n";
-    HT.Indexes();
-    cout<<endl;
+    while (var){
+        system("cls");
 
-    string el;
-    cout<<"Элемент на удаление:";
-    cin>>el;
-    HT.del(el);
-    HT.output();
+        cout<<"1.Вставка элемента.\n2.Вывод таблицы.\n3.Список индексов.\n4.Поиск элемента по ключу.\n5.Удаление элемента по ключу.\n0.Выход\n";
+        cout<<"Выберите действие:";
+        cin>>var;
 
+        switch(var){
+            case 1:{
+                string code,name;
+                cout<<"Введите код специальности:";
+                cin>>code;
+                cout<<"\nВведите название ВУЗа:";
+                cin>>name;
+                cout<<endl;
+                HT.push(name,code);
+                system("pause");
+                break;
+            }
+            case 2:{
+                HT.output();
+                system("pause");
+                break;
+            }
+            case 3:{
+                cout<<"\nСписок индексов\n";
+                HT.Indexes();
+                cout<<endl;
+                system("pause");
+                break;
+            }
+            case 4:{
+                HT.findEl();
+                system("pause");
+                break;
+            }
+            case 5:{
+
+                string el;
+                cout<<"Элемент на удаление:";
+                cin>>el;
+                HT.del(el);
+                system("pause");
+                break;
+            }
+            case 0:{
+                exit(0);
+                system("pause");
+                break;
+            }
+            default:{
+                cout<<"Выберите операцию из списка!\n";
+                system("pause");
+                break;
+            }
+        }
+    }
 
     return 0;
 }
